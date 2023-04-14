5bef133202f1a927690bf1e071f92a2c30f87854
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-14 23:51:02 +0530

    controller for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post , Get , Delete} from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    @Post('/upload')
+  
+  async uploadPost() {
+    
+
+    return `post created`;
+  }
+
+  @Get('/id')
+  
+  async viewPhoto(): Promise<any> {
+
+
+    return `viewed post`;
+  }
+
+  @Delete('/id')
+  
+  async DeletePhoto(): Promise<any> {
+    return `post deleted`;
+  }
+}
+
+
