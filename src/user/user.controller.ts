7e1c7da74378c2eb6eb5ff63ac06de186d9dd6fe
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-14 20:34:44 +0530

    controller for user created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get, Post, Patch, Param } from '@nestjs/common';
+
+
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    @Get('/username')
+    async getUserByUsername(@Param('username') username: string): Promise<any> {
+      return `username ${username}`;
+    }
+  
+    @Get('/userid')
+    async getUserByUserid(@Param('userid') userid: string): Promise<any> {
+  
+      return `userid = ${userid}`;
+    }
+  
+    @Post('/signup')
+    async createNewUser(): Promise<any> {
+
+      return `new user created`;
+    }
+  
+    
+    @Patch('/userid')
+    async updateUserDetails(): Promise<any> {
+
+      return `user updated`;
+    }
+  
+  
+    
+
+    
+}
