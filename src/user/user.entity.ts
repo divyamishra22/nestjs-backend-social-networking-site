b45879409ad09cb99a2baf9f99aa0769d65ca4fd
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-14 20:19:27 +0530

    entity for user created

---
+++ src/user/user.entity.ts
@@ -0,0 +1,27 @@
+import { MyBaseEntity } from "src/commons/base.entity";
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+
+@Entity()
+export class User extends MyBaseEntity {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  bio: string;
+
+}
+
