56d0682e81e978ea519e0abdb6902fd52d32d411
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-23 16:52:07 +0530

    feed modules initialised

---
+++ src/feed/feed.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Likes } from 'src/like/like.entity';
+import { Posts } from 'src/posts/posts.entity';
+
+@Injectable()
+export class FeedService {
+    async getFeedData(
+        feedsPhotos,
+        userId: string,
+      ): Promise<{ isAuthor: boolean; isLiked: boolean; photo: Posts }> {
+        return feedsPhotos.map((posts: Posts) => {
+          let isAuthor = false;
+          if (posts.userId === userId) {
+            isAuthor = true;
+          }
+          let isLiked = false;
+          posts.likes.map((like: Likes) => {
+            if (like.userId === userId) {
+              isLiked = true;
+            }
+          });
+          return { isAuthor, isLiked, posts};
+        });
+      }
+}
