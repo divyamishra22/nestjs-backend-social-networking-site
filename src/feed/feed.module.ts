bc76787d4c23206951d70e91fdbba9d1f1745d35
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-23 17:23:41 +0530

    error resolved

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -4,13 +4,14 @@ import { FeedService } from './feed.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikeModule } from 'src/like/like.module';
 import { UserModule } from 'src/user/user.module';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
     imports: [
         // TypeOrmModule.forFeature([Posts, User, Likes]), 
         //   forwardRef(() => AuthModule, ),
           // AuthModule,
-          forwardRef(()=>LikeModule),
+          forwardRef(()=>PostsModule),
           forwardRef(()=>UserModule)
       ],
       controllers: [FeedController],
