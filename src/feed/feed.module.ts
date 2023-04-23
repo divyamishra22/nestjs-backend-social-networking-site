e787277db69dee70922e44e99aab8d2791902854
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-23 17:15:12 +0530

    feed module initialised

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,4 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { FeedController } from './feed.controller';
+import { FeedService } from './feed.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LikeModule } from 'src/like/like.module';
+import { UserModule } from 'src/user/user.module';
 
-@Module({})
+@Module({
+    imports: [
+        // TypeOrmModule.forFeature([Posts, User, Likes]), 
+        //   forwardRef(() => AuthModule, ),
+          // AuthModule,
+          forwardRef(()=>LikeModule),
+          forwardRef(()=>UserModule)
+      ],
+      controllers: [FeedController],
+      providers: [FeedService, ],
+//       exports: [PostsService]
+ })
+
 export class FeedModule {}
