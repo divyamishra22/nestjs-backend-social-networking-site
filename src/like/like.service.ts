cc9b6eb6fa0a73de8bc9ce517f81ab18cc3c5547
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-24 20:41:10 +0530

    update to get count of likes, get likes on postid,

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -24,73 +24,51 @@ export class LikeService {
       .getOne();
   }
 
-  async findLikeBypostId( postId: string): Promise<Likes> {
-    const like = await this.likeRepository.findOne({ where: { postId: postId }});
-    return like
-  }
+  
 
-  // async unLikepost(like: Likes): Promise<void> {
-  //   return this.removeLike(like);
-  // }
-
-  // async addLike( userid: string, postId: string): Promise<Likes> {
-  //   //  const post =  await this.postservice.getPostByPostId(postId);
-    
-  //   const like = new Likes();
-  //   like.userId = userid;
-  //   like.postId = postId;
-
-  //  return await this.likeRepository.save(like);
-     
-   
-  // }
 
   async likepost(userid: string , postid: string): Promise<any>{
-    const likes = await this.findLikeByUserAndPhotoId(userid, postid)
-    if(likes.id)
-    {const like = new Likes();
+    const likes = await this.findLikeByUserAndPostId(userid, postid)
+    if(!likes)
+    {
+      const like = new Likes();
     like.userId = userid;
     like.postId = postid;
-    return await this.likeRepository.save(like);}
+    return await this.likeRepository.save(like);
+  }
     else{
       throw new ConflictException('Like already exists');
     }
   }
 
-  async findLikeByUserAndPhotoId(
-    userId: string,
-    postId: string,
-  ): Promise<any> {
-    
-   const likes= await this.findLikeBypostId(postId)
-   if(likes)
-    {return await this.likeRepository.createQueryBuilder('like')
-      .where('like.userId = :userId AND like.postId = :postId', {
-        userId,
-        postId,
-      })
-      .getOne();}
-    else{
-      throw new NotFoundException('Likes not found');
+  
+
+  async deleteLikeById(postid, userid ): Promise<any> {
+    const like = await this.findLikeByUserAndPostId(userid, postid);
+    if(like)
+    {
+    if (like.userId !== userid) {
+      throw new UnauthorizedException('Unauthorized');
     }
-      
-  }
-
-  // async removeLike(like: string): Promise<void> {
-  //  await this.likeRepository.remove(like);
-  // }
-
-  async deleteLikeById(id: string, ): Promise<any> {
-    // const like = await this.findLikeByUserAndPostId(id, userid);
-    // if (like.userId !== userid) {
-    //   throw new UnauthorizedException('Unauthorized');
-    // }
-   
+   else{
+     const id = like.id
     const result = await this.likeRepository.delete({ id });
     return result.affected === 1;
-    // if (result.affected === 0) {
-    //   throw new NotFoundException(`Post not found`);
-    // }
+   }
+  }
+    else {
+      throw new NotFoundException(`Post not found`);
+    }
+  }
+
+  async getlikesonpostId(postid): Promise<any>{
+    return await this.likeRepository.createQueryBuilder('likes')
+    .where('likes.postId = :postid', {
+      postid,
+    })
+    .getCount(); 
+
+
   }
 }
 
