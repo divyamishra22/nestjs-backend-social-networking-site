86c542d95b702b87c4e2b6fd43c00753cde7c00b
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-29 01:35:50 +0530

    entity & realtion of comment created

---
+++ src/comments/comments.entity.ts
@@ -0,0 +1,27 @@
+import { MyBaseEntity } from "src/commons/base.entity";
+import { Posts } from "src/posts/posts.entity";
+import { User } from "src/user/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+
+
+@Entity()
+export class Comment extends MyBaseEntity {
+ 
+  @Column()
+  body: string;
+
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => User, (user) => user.comments, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' , })
+  user: User;
+
+  @ManyToOne(() => Posts, (posts) => posts.comments, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'postId' , })
+  posts: Posts;
+
+  @Column()
+  postId: string;
+
+}
