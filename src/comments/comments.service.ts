c6b6d5a65c1ee2c4acd1464555b523f1fcf5d324
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-29 11:32:01 +0530

    getcomment byid , delete comment added

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CommentRepository } from './comments.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Comments } from './comments.entity';
@@ -21,6 +21,22 @@ export class CommentsService {
         comment.postId = postId;
         await this.commentRepository.save(comment);
         return await this.commentRepository.findOne({where:{id: comment.id}});
+      }
+
+
+      async getCommentById(id: string): Promise<Comments> {
+        const comment = await this.commentRepository.findOne({where:{id: id}});
+        if (!comment) {
+          throw new NotFoundException('Comment not Found');
+        }
+        return comment;
+      }
+    
+      async deleteComment(comment: Comments, userId: string): Promise<any> {
+        if (comment.userId !== userId) {
+          throw new UnauthorizedException('Unauthorized');
+        }
+        return this.commentRepository.delete({id: comment.id});
       }
 
 
