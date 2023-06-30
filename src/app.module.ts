801e5b3869bfde300d658ef6b8f29d5009185349
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-06-30 21:20:07 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,45 +4,49 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
-import { PostsController } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
 import { PostsModule } from './posts/posts.module';
 import { Posts } from './posts/posts.entity';
-import { LikeController } from './like/like.controller';
-import { LikeService } from './like/like.service';
 import { LikeModule } from './like/like.module';
 import { Likes } from './like/like.entity';
-// import { Like } from 'typeorm';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
-import { FollowController } from './follow/follow.controller';
-import { FollowService } from './follow/follow.service';
 import { FollowModule } from './follow/follow.module';
 import { Follow } from './follow/follow.entity';
-import { FeedService } from './feed/feed.service';
-import { FeedController } from './feed/feed.controller';
 import { FeedModule } from './feed/feed.module';
 // import { CloudinaryModule } from './cloudinary/cloudinary.module';
-import { CommentsController } from './comments/comments.controller';
-import { CommentsService } from './comments/comments.service';
 import { CommentsModule } from './comments/comments.module';
 import { Comments } from './comments/comments.entity';
+import * as dotenv from 'dotenv'
+
+
+require('dotenv').config();
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      //host: 'localhost',
-      //
-      port: 5432,
-      username: 'postgres',
-      password: 'divya123',
-      database: 'mydb1',
-      entities: [User, Posts, Likes, Follow, Comments],
+    //   type: 'postgres',
+    //   //host: 'localhost',
+    //   //
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'divya123',
+    //   database: 'mydb1',
+    //   entities: [User, Posts, Likes, Follow, Comments],
+    //   synchronize: true,
+    //   logger:'advanced-console',
+    //   logging: 'all',  
+
+    type: 'postgres' ,
+    host: process.env.HOST,
+  port: parseInt(process.env.PORT),
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+     entities: [User, Posts, Likes, Follow, Comments],
       synchronize: true,
-      logger:'advanced-console',
+        logger:'advanced-console',
       logging: 'all',  
+  
     }),
     UserModule,
     PostsModule,
@@ -52,6 +56,8 @@ import { Comments } from './comments/comments.entity';
     FeedModule,
     CommentsModule,
     // CloudinaryModule
+
+   
   ],
   controllers: [AppController,  ],
   providers: [AppService, ],
